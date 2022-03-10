83547f4a7ea18f9239a8ad1e955c22f44d24c511
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-09 22:57:54 -0600

    moved google font call to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,16 @@ export default function Document() {
                 <link rel='manifest' href='/manifest.webmanifest' />
                 <link rel='apple-touch-icon' href='/images/isometric-sq.png' />
                 <link rel='icon' href='/images/isometric.ico' />
+                <link rel='preconnect' href='https://fonts.googleapis.com' />
+                <link
+                    rel='preconnect'
+                    href='https://fonts.gstatic.com'
+                    crossOrigin=''
+                />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap'
+                    rel='stylesheet'
+                />
             </Head>
             <body>
                 <Main />
