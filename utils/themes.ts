5ec728c8c41eff0f76c22413e380543283ce64c1
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-08-27 14:41:09 -0500

    fixed theme meta

--- utils/themes.ts
+++ utils/themes.ts
@@ -1,7 +1,7 @@
 const themes = new Map([
   ['default', { light: '#bdcadb', dark: '#091024', displayName: 'Default' }],
   ['edge', { light: '#CCCCCC', dark: '#14171A', displayName: 'Edge' }],
-  ['lime', { light: '#CCCCCC', dark: '#14171A', displayName: 'Lime' }],
+  ['lime', { light: '#c2d6cc', dark: '#0b0d0e', displayName: 'Lime' }],
 ]);
 
 export default themes;
