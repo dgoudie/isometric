f5e5471bd0db4fb6ba9c8b7fdf726ec18702b684
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-09-05 23:16:19 -0500

    fixed fetcher error

--- utils/fetch-with-403-redirect.ts
+++ utils/fetch-with-403-redirect.ts
@@ -1,11 +1,11 @@
 import { useCallback } from 'react';
 import { useRouter } from 'next/router';
 
-export const useFetchJSONWith403Redirect = () => {
+export const useFetchJSONWith403Redirect = <T = any>() => {
   const router = useRouter();
 
   const fetcher = useCallback(
-    <T = any>(...args: Parameters<typeof fetch>) =>
+    (...args: Parameters<typeof fetch>) =>
       fetch(...args).then((res) => {
         if (res.status === 403) {
           router.replace('/');
