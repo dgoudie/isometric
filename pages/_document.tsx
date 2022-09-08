fc221462f3d7bc1d673e1bfbcd63b567fff5e2f1
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-09-07 23:45:31 -0500

    updated meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,20 +4,99 @@ import Script from 'next/script';
 import themes from '../utils/themes';
 
 export default function Document() {
+  const description =
+    'ISOMETRIC is a simple app to record your workouts. ISOMETRIC can help you to keep track of important data points such as repetitions, resistance, and time for each set during your workout.';
   return (
     <Html>
       <Head>
+        <meta name='description' content={description} />
+        <meta property='og:title' content='ISOMETRIC' />
+        <meta property='og:site_name' content='ISOMETRIC' />
+        <meta property='og:url' content='https://isometric.goudie.dev' />
+        <meta property='og:description' content={description} />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:image'
+          content='https://isometric.goudie.dev/images/isometric.png'
+        />
+        <meta name='twitter:card' content='app' />
+        <meta name='twitter:creator' content='@daniel_j_goudie' />
+        <meta name='twitter:description' content={description} />
         <meta charSet='utf-8' />
         <link
           rel='icon'
           href='/images/isometric.ico'
           crossOrigin='use-credentials'
         />
+        {/* Generated from pwa asset generator */}
         <link
-          rel='apple-touch-icon'
-          href='/images/isometric.png'
-          crossOrigin='use-credentials'
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-2048-2732.jpg'
+          media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
         />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1668-2388.jpg'
+          media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1536-2048.jpg'
+          media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1668-2224.jpg'
+          media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1620-2160.jpg'
+          media='(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1284-2778.jpg'
+          media='(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1170-2532.jpg'
+          media='(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1125-2436.jpg'
+          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1242-2688.jpg'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-828-1792.jpg'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-1242-2208.jpg'
+          media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-750-1334.jpg'
+          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link
+          rel='apple-touch-startup-image'
+          href='/icons/apple-splash-640-1136.jpg'
+          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        <link rel='apple-touch-icon' href='/icons/apple-icon-180.png' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        {/* End Generated from pwa asset generator */}
         <meta name='color-scheme' content='light'></meta>
         <meta name='theme-color' content='#bdcadb' />
         <meta
