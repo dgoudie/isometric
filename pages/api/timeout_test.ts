4059447343fc0279257a31e110cb55aca87f6ac3
dgoudie <34222208+dgoudie@users.noreply.github.com>
2023-02-28 23:44:36 -0600

    adding edge function for a test

---
+++ pages/api/timeout_test.ts
@@ -0,0 +1,26 @@
+import type { NextFetchEvent, NextRequest } from 'next/server';
+
+import { NextResponse } from 'next/server';
+
+export const config = {
+  runtime: 'edge',
+};
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function getAlbum() {
+  await wait(120000);
+  const res = await fetch('https://jsonplaceholder.typicode.com/albums/1');
+  return res.json();
+}
+
+export default function MyEdgeFunction(
+  request: NextRequest,
+  context: NextFetchEvent
+) {
+  context.waitUntil(getAlbum().then((json) => console.log({ json })));
+
+  return NextResponse.json({
+    name: `Hello, from ${request.url} I'm an Edge Function!`,
+  });
+}
