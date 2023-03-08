af607484a76f478f125999c4e077ef13444ce61d
dgoudie <34222208+dgoudie@users.noreply.github.com>
2023-03-07 21:02:10 -0600

    fixed VAPID edge function

--- pages/api/push/vapid_public_key.ts
+++ pages/api/push/vapid_public_key.ts
@@ -1,18 +1,5 @@
-import type { NextRequest, NextResponse } from 'next/server';
-
-const handler = async (req: NextRequest, res: NextResponse) => {
-  const response = await fetch(
-    `${process.env.NOTIFICATION_HOST}/vapid_public_key`
-  );
-  if (!response.ok) {
-    return response;
-  }
-  const clonedResponse = response.clone();
-  clonedResponse.headers.append(
-    'cache-control',
-    's-maxage=1, stale-while-revalidate=59'
-  );
-  return clonedResponse;
+const handler = async () => {
+  return fetch(`${process.env.NOTIFICATION_HOST}/vapid_public_key`);
 };
 
 export const config = {
